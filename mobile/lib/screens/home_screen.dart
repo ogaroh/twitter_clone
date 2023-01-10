570bd584ce4b191ddd4569e7bd1b182393fcb137
Orket <developerorket@gmail.com>
2023-01-10 17:21:48 +0300

    drawer progress

--- mobile/lib/screens/home_screen.dart
+++ mobile/lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mobile/constants/colors.dart';
 import 'package:mobile/theme.dart';
+import 'package:mobile/widgets/drawer.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -12,76 +13,103 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        drawer: DrawerWidget(),
         // extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          elevation: 0,
-          title: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                CircleAvatar(
-                  backgroundColor: kBlack,
-                ),
-                SvgPicture.asset(
-                  'assets/svgs/twitter_logo.svg',
-                  height: 30,
-                ),
-                IconButton(
-                  icon: Icon(
-                    CupertinoIcons.sparkles,
-                    color: kBlack,
-                  ),
-                  onPressed: (() {
-                    showModalBottomSheet(
-                        backgroundColor: Colors.transparent,
-                        context: context,
-                        builder: ((context) {
-                          return Container(
-                            padding: EdgeInsets.all(20),
-                            height: MediaQuery.of(context).size.height * 0.4,
-                            decoration: BoxDecoration(
-                                color: kWhite,
-                                borderRadius: BorderRadius.circular(30)),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Your timeline',
-                                  style: myHeader1Style,
-                                ),
-                                ListTile(
-                                  leading: Icon(CupertinoIcons.sparkles),
-                                  title: Text('For you'),
-                                  subtitle: Text('Show recommended tweets'),
-                                  trailing: Radio(
-                                      value: true,
-                                      groupValue: null,
-                                      onChanged: ((value) {})),
-                                ),
-                                ListTile(
-                                  leading: Icon(Icons.history),
-                                  title: Text('Latest'),
-                                  subtitle: Text(
-                                      'Show the newest tweets from accounts you follow'),
-                                  trailing: Radio(
-                                      value: true,
-                                      groupValue: null,
-                                      onChanged: ((value) {})),
-                                )
-                              ],
-                            ),
-                          );
-                        }));
-                  }),
-                )
-              ],
-            ),
-          ),
-          backgroundColor: kWhite.withAlpha(200),
-        ),
+
         body: Column(
           children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20, vertical: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Builder(builder: (context) {
+                    return GestureDetector(
+                      onTap: (() {
+                        Scaffold.of(context).openDrawer();
+                      }),
+                      child: const CircleAvatar(
+                        backgroundColor: kBlack,
+                      ),
+                    );
+                  }),
+                  SvgPicture.asset(
+                    'assets/svgs/twitter_logo.svg',
+                    height: 30,
+                  ),
+                  IconButton(
+                    icon: const Icon(
+                      CupertinoIcons.sparkles,
+                      color: kBlack,
+                    ),
+                    onPressed: (() {
+                      showModalBottomSheet(
+                          backgroundColor: Colors.transparent,
+                          context: context,
+                          builder: ((context) {
+                            return Container(
+                              padding: const EdgeInsets.all(20),
+                              height: MediaQuery.of(context).size.height * 0.4,
+                              decoration: BoxDecoration(
+                                  color: kWhite,
+                                  borderRadius: BorderRadius.circular(30)),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Your timeline',
+                                    style: myHeader1Style,
+                                  ),
+                                  const SizedBox(
+                                    height: 20,
+                                  ),
+                                  RichText(
+                                      text: const TextSpan(
+                                          style: TextStyle(
+                                              color: kBlack, fontSize: 14),
+                                          text:
+                                              'Choose how you want to see your timeline. Make more changes in your ',
+                                          children: [
+                                        TextSpan(
+                                            text: 'Content preferences',
+                                            style: TextStyle(
+                                                decoration:
+                                                    TextDecoration.underline,
+                                                color: kBlack,
+                                                fontWeight: FontWeight.bold))
+                                      ])),
+                                  ListTile(
+                                    leading:
+                                        const Icon(CupertinoIcons.sparkles),
+                                    title: const Text('For you'),
+                                    subtitle:
+                                        const Text('Show recommended tweets'),
+                                    trailing: Radio(
+                                        value: true,
+                                        groupValue: null,
+                                        onChanged: ((value) {})),
+                                  ),
+                                  ListTile(
+                                    leading: const Icon(Icons.history),
+                                    title: const Text('Latest'),
+                                    subtitle: const Text(
+                                        'Show the newest tweets from accounts you follow'),
+                                    trailing: Radio(
+                                        value: true,
+                                        groupValue: null,
+                                        onChanged: ((value) {})),
+                                  )
+                                ],
+                              ),
+                            );
+                          }));
+                    }),
+                  )
+                ],
+              ),
+            ),
             const Center(child: Text('home')),
           ],
         ),
